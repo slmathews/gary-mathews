dc9c1836195a3b79c256f733e9ac1fa41f8d52e1
Kyle Mathews <mathews.kyle@gmail.com>
2016-08-03 12:37:45 -0600

    initial commit

---
+++ pages/index.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Link } from 'react-router'
+import { prefixLink } from 'gatsby-helpers'
+import DocumentTitle from 'react-document-title'
+import { config } from 'config'
+
+// Styles for highlighted code blocks.
+import 'css/zenburn.css'
+
+export default class Index extends React.Component {
+  render () {
+    return (
+      <DocumentTitle title={config.siteTitle}>
+        <div>
+          <h1>
+            Hi people
+          </h1>
+          <p>Welcome to the Gary Mathews' Biography</p>
+        </div>
+      </DocumentTitle>
+    )
+  }
+}
